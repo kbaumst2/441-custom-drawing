88dbd356bf3b95cc5f066c51776089c01ee2cdef
Kate Baumstein <kate.baumstein@ibm.com>
2021-03-03 09:57:27 -0500

    works for swift

--- drawing/drawing/AlienView.swift
+++ drawing/drawing/AlienView.swift
@@ -10,18 +10,45 @@ import UIKit
 
 class AlienView: UIView{
     
+    var alienx = 0
+    var alieny = 0
     
     override func draw(_ rect: CGRect) {
-        let context = UIGraphicsGetCurrentContext()
-        //self.backgroundColor = UIColor.blue
-        context?.setFillColor(UIColor.blue.cgColor)
+//        let context = UIGraphicsGetCurrentContext()
+//        //self.backgroundColor = UIColor.blue
+//        context?.setFillColor(UIColor.blue.cgColor)
+//
+//
+//        print("DRAWIN")
+//        context!.setLineWidth(2.0)
+//        context!.setStrokeColor(UIColor.blue.cgColor)
+//        context?.move(to: CGPoint(x: 0, y: self.frame.size.height))
+//        context?.addLine(to: CGPoint(x: self.frame.size.width, y: 0))
+//        context!.strokePath()
         
+        let color: UIColor = UIColor.yellow
+        let drect =  CGRect(x: alienx, y:alieny, width: 50, height: 50)
+        let bpath: UIBezierPath = UIBezierPath(rect: drect)
+        color.set()
+        bpath.stroke()
+        UIColor.blue.set()
+        UIRectFill(drect)
         
-        print("DRAWIN")
-        context!.setLineWidth(2.0)
-        context!.setStrokeColor(UIColor.blue.cgColor)
-        context?.move(to: CGPoint(x: 0, y: self.frame.size.height))
-        context?.addLine(to: CGPoint(x: self.frame.size.width, y: 0))
-        context!.strokePath()
     }
+    
+    @objc func update(){  //do collision detection here
+        alienx += 1
+        alieny += 1
+        setNeedsDisplay()  //calls draw function (like a flag)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches {
+            let point = t.location(in: self)
+            alienx = Int(point.x)
+            alieny = Int(point.y)
+            
+        }
+    }
+    
 }
--- drawing/drawing/ViewController.swift
+++ drawing/drawing/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var alienView: AlienView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //when screen needs to refresh, calls this selector
+        let displayLink = CADisplayLink(target: self, selector: #selector(update))
+        displayLink.add(to: RunLoop.main, forMode: RunLoop.Mode.default)
+    }
+    
+    @objc func update(){
+        //print(update())
+        print("update")
+        alienView.update()
     }
 
 
